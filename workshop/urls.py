e80f9de977ee292cace9c24547cb09955ca733b7
Jhonatan Laguna <hola@jhonatanlaguna.com>
2018-04-03 18:44:27 -0700

    Urls

--- workshop/urls.py
+++ workshop/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+
+from rest_framework.documentation import include_docs_urls
+
+from workshop import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'docs/', include_docs_urls(title='Todo API', description='RESTful API for Todo')),
+
+    re_path(r'^$', views.api_root),
+    re_path(r'^', include('users.urls', namespace='users')),
+    re_path(r'^', include('todos.urls', namespace='todos')),
 ]
